44dc72ac9532b7636af9de531042ffd87a506afe
MrHitori <ipokewar@gmail.com>
2016-05-29 17:03:19 +0300

    Problems with Left/Top

--- rect.js
+++ rect.js
@@ -18,16 +18,18 @@ function addBorder(){ this.style.border = "1px solid #000"; this.style.zIndex = 
 function removeBorder(){ this.style.border = "1px solid transparent"; this.style.zIndex = "1"; }
 
 function createDivs(){
+ client_w = document.body.clientWidth;
+ client_h = document.body.clientHeight;
  for(var i=0;i<10;i++){
-  var randWidth = Math.floor(Math.random() * (screen.width / 3)) + 10;
-  var randHeight = Math.floor(Math.random() * (screen.height / 3)) + 10;
+  var randWidth = Math.floor(Math.random() * (client_w / 3)) + 10;
+  var randHeight = Math.floor(Math.random() * (client_h / 3)) + 10;
 
   var randR = Math.floor(Math.random() * 255) + 1;
   var randG = Math.floor(Math.random() * 255) + 1;
   var randB = Math.floor(Math.random() * 255) + 1;
 
-  var randTop = Math.floor(Math.random() * (screen.height) - randHeight) - 1;
-  var randLeft = Math.floor(Math.random() * (screen.width) - randWidth) - 1;
+  var randTop = Math.floor(Math.random() * (client_h)); if(randTop>randHeight){ randTop-=randHeight; }
+  var randLeft = Math.floor(Math.random() * (client_w)); if(randLeft>randWidth){ randLeft-=randWidth; }
 
   var div = document.createElement('div');
   div.className = "rect";
